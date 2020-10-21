fd6e22af2982b6881f92961698fd2626c0971546
lrahal <line_r@hotmail.fr>
2020-10-21 18:02:59 +0200

    Work

--- notebooks/scripts/fab_sites_map.py
+++ notebooks/scripts/fab_sites_map.py
@@ -28,7 +28,8 @@ df_sites = get_filtered_dataframe()
 # site(s) de contrôle
 # site(s) d'échantillothèque
 # site(s) de certification
-site_name = 'site(s) de production  / sites de production alternatif(s)'
+# site(s) de fabrication de la substance active
+site_name = 'site(s) de fabrication de la substance active'
 
 # Add site to dataframe columns
 df_sites = add_selected_site(df_sites, site_name=site_name)
@@ -40,118 +41,232 @@ df_sites = add_selected_site(df_sites, site_name=site_name)
 df_sites.head(1)
 
 
-# In[4]:
+# In[ ]:
 
 
-all_cis = df_sites.cis.unique()   # 4982
-bad_cis = df_sites[df_sites.cis.apply(lambda x: isinstance(x, str))].cis.unique()   # 381
-
-
-# In[5]:
-
+# all_cis = df_sites.cis.unique()   # 4982
+# bad_cis = df_sites[df_sites.cis.apply(lambda x: isinstance(x, str))].cis.unique()   # 381
 
 # Keep rows having integer cis (optional)
 # df_sites = df_sites[df_sites.cis.apply(lambda x: isinstance(x, int))].copy()
 
 
-# In[6]:
+# In[4]:
 
 
 len(df_sites)
 
 
-# In[7]:
+# In[5]:
 
 
 nb_addresses = len(df_sites.site_name.unique())
+print(nb_addresses)
 
 
 # # Get country
 
+# In[7]:
+
+
+# get_locations(df_sites, output_filename='sites_fab_api.csv', nb_addresses=nb_addresses)
+
+
 # In[8]:
 
 
-get_countries(df_sites, nb_addresses=200)
+df_sites_sample = df_sites.iloc[:, :]
 
 
-# In[9]:
+# # API fabrication sites map
 
-
-df_sites_sample = df_sites.iloc[:200, :]
-
+# ## API fabrication sites by cis (speciality)
 
 # In[10]:
 
 
-#df_sites['country'] = df_sites.apply(
-#    lambda x: get_google_results(x.site_name)['country'], axis=1
-#)
+df_loc = pd.read_csv('/Users/linerahal/Documents/GitHub/datamed/data/sites_fab_api.csv')
 
 
 # In[11]:
 
 
-df_sites_sample.head(1)
+df_loc = df_loc.drop(columns=['Unnamed: 0'])
+df_loc = df_loc.rename(columns={'input_string': 'site_name'})
 
 
-# # Production sites map
-
-# ## Production sites by cis (speciality)
-
-# In[18]:
-
-
-df_addresses = pd.read_csv('/Users/linerahal/Documents/GitHub/datamed/data/output-addresses.csv')
-
-
-# In[19]:
-
-
-df_addresses = df_addresses.drop(columns=['Unnamed: 0'])
-df_addresses = df_addresses.rename(columns={'input_string': 'site_name'})
-
-
-# In[35]:
+# In[12]:
 
 
 df = pd.merge(
     df_sites_sample[['dénomination de la spécialité', 'cis', 'substance active', 'site_name']],
-    df_addresses,
+    df_loc,
     on='site_name',
     how='inner')
 
 
-# In[36]:
-
-
-len(df)
-
-
-# In[37]:
+# In[13]:
 
 
 df.head(1)
 
 
-# In[38]:
+# In[14]:
 
 
 api_list = df['substance active'].unique().tolist()
 
 
-# In[39]:
+# In[15]:
 
 
 len(api_list)
 
 
-# In[40]:
+# In[16]:
 
 
 df['substance active'].value_counts()
 
 
-# In[45]:
+# In[17]:
+
+
+len(df)
+
+
+# In[27]:
+
+
+# Retirer les geocodings qui n'ont pas fonctionné (35 sur 8940)
+df = df[(df.status != 'ZERO_RESULTS') & (df.status != 'INVALID_REQUEST') & (~df.country.isna())]
+len(df)
+
+
+# In[ ]:
+
+
+# list(df['substance active'])
+df_api = pd.DataFrame(df['substance active'].unique(), columns=['substance active'])   #.to_csv('substances_actives.csv', index=False)
+
+
+# In[ ]:
+
+
+df_api.to_csv('substances_actives.csv', index=False)
+
+
+# ## API fabrication sites by active substances (api)
+
+# In[28]:
+
+
+df[df['country'].isna()]
+
+
+# In[29]:
+
+
+# import the plotly express
+import plotly.express as px
+
+# set up the chart from the df dataFrame
+fig = px.scatter_geo(df, 
+                     # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
+                     lon='longitude', 
+                     lat='latitude',
+                     # style
+                     color='country',
+                     # choose the map chart's projection
+                     projection='natural earth',
+                     # columns which is in bold in the pop-up
+                     hover_name = 'substance active',
+                     # format of the pop-up not to display these columns' data
+                     hover_data = {'substance active': False, 'longitude': False, 'latitude': False}
+                    )
+#fig.update_traces(marker=dict(size=10, color='red'))
+fig.update_traces(marker=dict(size=10))
+fig.update_geos(fitbounds='locations', showcountries=True)
+fig.update_layout(title = 'Production sites')
+fig.show()
+
+
+# ## Number of api producted by country
+
+# In[30]:
+
+
+grouped_df = df.groupby('country')
+grouped_df = grouped_df.agg({'substance active': 'nunique'})
+grouped_df = grouped_df.reset_index()
+
+
+# In[31]:
+
+
+grouped_df['site_name'] = grouped_df.country
+
+
+# In[32]:
+
+
+# get_locations(grouped_df, output_filename='countries_locations.csv', nb_addresses=len(grouped_df))
+
+
+# In[33]:
+
+
+df_countries_loc = pd.read_csv('/Users/linerahal/Documents/GitHub/datamed/data/countries_locations.csv')
+
+
+# In[34]:
+
+
+df_countries_loc = df_countries_loc.drop(columns=['Unnamed: 0'])
+df_countries_loc = df_countries_loc.rename(columns={'input_string': 'site_name'})
+
+
+# In[35]:
+
+
+df_countries = pd.merge(grouped_df[['substance active', 'site_name']], df_countries_loc, on='site_name', how='inner')
+
+
+# In[36]:
+
+
+df_countries = df_countries[~df_countries.country.isna()]
+
+
+# In[44]:
+
+
+# import the plotly express
+import plotly.express as px
+
+# set up the chart from the df dataFrame
+fig = px.scatter_geo(df_countries, 
+                     # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
+                     lon='longitude', 
+                     lat='latitude',
+                     # style
+                     color='country',
+                     size='substance active',
+                     # choose the map chart's projection
+                     projection='natural earth',
+                     # columns which is in bold in the pop-up
+                     hover_name = 'country',
+                     # format of the pop-up not to display these columns' data
+                     hover_data = {'country': False, 'longitude': False, 'latitude': False}
+                    )
+fig.update_geos(fitbounds='locations', showcountries=True)
+fig.update_layout(title = 'Active substances fabrication sites')
+fig.show()
+
+
+# ## Repartition of API fabrication sites by particular API
+
+# In[41]:
 
 
 # import the plotly express
@@ -174,14 +289,8 @@ fig = px.scatter_geo(df[df['substance active'] == 'hydrochlorothiazide'],
 #fig.update_traces(marker=dict(size=10, color='red'))
 fig.update_traces(marker=dict(size=10))
 fig.update_geos(fitbounds='locations', showcountries=True)
-fig.update_layout(title = 'Production sites')
+fig.update_layout(title = 'Fabrication sites of hydrochlorothiazide')
 fig.show()
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
