40a904eabce3288850e2f20bc19aac72c732fce2
lrahal <line_r@hotmail.fr>
2020-10-26 17:24:53 +0100

    Refacto

--- jade_analysis.py
+++ jade_analysis.py
@@ -5,54 +5,18 @@ import pandas as pd
 PATH = '/Users/linerahal/Desktop/DataMed/RS/JADE/'
 
 
-def clean_data(df):
+def clean_column(col):
     """
-    Data cleaning
-    Lower case + remove spaces
-    :param df: DataFrame (raw data)
-    :return: DataFrame
+    Clean dataframe columns content
+    :param col: DataFrame column
+    :return: cleaned column
     """
-    df = df.apply(lambda x: x.astype(str).str.lower())
-    df = df.apply(lambda x: x.astype(str).str.strip())
-    df = df.apply(lambda x: x.astype(str).str.replace('\n', ' '))
-    df = df.applymap(lambda x: re.sub(' +', ' ', x) if isinstance(x, str) else x)
-    df.columns = df.columns.str.lower()
-    df.columns = df.columns.str.strip()
-    df.columns = df.columns.str.replace('\n', ' ')
-    return df
-
-
-def get_dataframe():
-    """
-    List all files in JADE directory
-    Create an only DataFrame containing info of all the files
-    :return: DataFrame
-    """
-    files = os.listdir(PATH)
-    files = files[1:]   # Remove DS_Store file
-    df = pd.concat((pd.read_excel(PATH + f) for f in files), axis=0, ignore_index=True)
-    clean_df = clean_data(df)
-    return clean_df
-
-
-def get_dataset_columns():
-    files = os.listdir(PATH)
-    files = files[1:]   # Remove DS_Store file
-    columns_list = []
-    for f in files:
-        df_file = pd.read_excel(PATH + f)
-        df_file = clean_data(df_file)
-        file_dict = {'filename': f}
-        file_dict.update({'col_' + str(i+1): col for i, col in enumerate(df_file.columns)})
-        columns_list.append(file_dict)
-    columns = ['filename'] + ['col_' + str(i) for i in range(1, max(map(len, columns_list)))]
-    df = pd.DataFrame(columns_list, columns=columns)
-    return df
+    return col.astype(str).str.lower().str.strip().str.replace('\n', ' ')
 
 
 def convert_cis(cis_str):
     """
-    Keep only integer cis codes
+    Convert cis codes in int type if possible
     """
     if cis_str.replace(' ', '').replace('.', '').replace('cis', '').replace(':', '').isdigit():
         try:
@@ -64,33 +28,22 @@ def convert_cis(cis_str):
     return cis_int
 
 
-def get_filtered_dataframe():
+def clean_data(df):
     """
-    Choose interesting columns (corresponding to most recent files)
-    Filter on files containing those columns
-    Create big dataframe with all those files
-    Clean the dataframe
+    Data cleaning
+    Lower case + remove spaces
+    :param df: DataFrame (raw data)
     :return: DataFrame
     """
-    cols = ['dénomination de la spécialité', 'cis', 'dénomination commune (dci)', "type d'amm", "titulaire de l'amm",
-            'site(s) de production  / sites de production alternatif(s)', 'site(s) de conditionnement primaire',
-            'site(s) de conditionnement secondaire', "site d'importation", 'site(s) de contrôle',
-            "site(s) d'échantillothèque", 'site(s) de certification', 'substance active',
-            'site(s) de fabrication de la substance active', 'mitm (oui/non)', 'pgp (oui/non)']
+    # Global cleaning
+    df[df.columns] = df[df.columns].apply(clean_column)
+    df = df.applymap(lambda x: re.sub(' +', ' ', x) if isinstance(x, str) else x)
+    df.columns = df.columns.str.lower().str.strip().str.replace('\n', ' ')
+    df.cis = df.cis.apply(lambda x: convert_cis(x))
 
-    df_cols = get_dataset_columns()
-    df_cols['cols_ok'] = df_cols.apply(lambda x: list(x)[1:17] == cols, axis=1)
-
-    filenames = list(df_cols[df_cols.cols_ok].filename)
-    df_data = pd.concat((pd.read_excel(PATH + f) for f in filenames), axis=0, ignore_index=True, sort=False)
-    df_data = df_data[
-        (~df_data['Dénomination de la spécialité'].isna())
-        & (df_data['Dénomination de la spécialité'] != 'une ligne par dénomination et par susbtance active')
-    ]
-    clean_df = clean_data(df_data)
-    clean_df = clean_df.iloc[:, :-23]
-    clean_df.cis = clean_df.cis.apply(lambda x: convert_cis(x))
-    clean_df = clean_df.rename(
+    # Select only 17 first columns
+    df = df.iloc[:, :-23]
+    df = df.rename(
         columns={'dénomination de la spécialité': 'denomination_specialite',
                  'dénomination commune (dci)': 'dci',
                  "type d'amm": 'type_amm',
@@ -107,12 +60,59 @@ def get_filtered_dataframe():
                  'mitm (oui/non)': 'mitm',
                  'pgp (oui/non)': 'pgp'
                  })
+    return df
+
+
+def get_good_filenames():
+    """
+    Get the list of files sharing the same structure (given in cols list)
+    Upgrade: should find a way to load only the header of the Excel file
+    :return: list
+    """
+    files = os.listdir(PATH)
+    files = files[1:]   # Remove DS_Store file
+
+    cols = ['dénomination de la spécialité', 'cis', 'dénomination commune (dci)', "type d'amm", "titulaire de l'amm",
+            'site(s) de production  / sites de production alternatif(s)', 'site(s) de conditionnement primaire',
+            'site(s) de conditionnement secondaire', "site d'importation", 'site(s) de contrôle',
+            "site(s) d'échantillothèque", 'site(s) de certification', 'substance active',
+            'site(s) de fabrication de la substance active', 'mitm (oui/non)', 'pgp (oui/non)']
+
+    filenames = []
+    for f in files:
+        df_file = pd.read_excel(PATH + f)
+        df_file.columns = df_file.columns.str.lower().str.strip().str.replace('\n', ' ')   # Clean column title
+        if list(df_file.columns)[:16] == cols:
+            filenames.append(f)
+    return filenames
+
+
+def get_filtered_dataframe():
+    """
+    Choose interesting columns (corresponding to most recent files)
+    Filter on files containing those columns
+    Create big dataframe with all those files
+    Clean the dataframe
+    :return: DataFrame
+    """
+    # Get filenames having good structure
+    filenames = get_good_filenames()
+    # Create dataframe
+    df_data = pd.concat((pd.read_excel(PATH + f) for f in filenames), axis=0, ignore_index=True, sort=False)
+    # Remove rows having bad information
+    df_data = df_data[
+        (~df_data['Dénomination de la spécialité'].isna())
+        & (df_data['Dénomination de la spécialité'] != 'une ligne par dénomination et par susbtance active')
+    ]
+    # Clean dataframe
+    clean_df = clean_data(df_data)
     return clean_df
 
 
 def add_selected_site(df, site_name='site(s) de production  / sites de production alternatif(s)'):
     """
     Retrieve the first address mentioned for site name
+    Multiple addresses are separated by ';'
     :param df: DataFame (= clean DataFrame)
     :return: DataFrame
     ex: site_name = 'site(s) de production  / sites de production alternatif(s)'
