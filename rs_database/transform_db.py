4dabe72bb920a25e69e9df15178a6cb4971d2201
lrahal <line_r@hotmail.fr>
2020-10-29 18:06:52 +0100

    Work

--- rs_database/transform_db.py
+++ rs_database/transform_db.py
@@ -1,20 +1,23 @@
-import nltk
 import string
+import unidecode
+import numpy as np
 from nltk.corpus import stopwords
+from typing import Tuple, List, Dict, DefaultDict
+from upload_db import upload_fab_sites, get_api_by_cis
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
-from upload_db import upload_fab_sites, get_api_by_cis
 
-STOPWORDS = nltk.corpus.stopwords.words('french')
+STOPWORDS = stopwords.words('french')
 
 
 class FabricationSites:
     def __init__(self):
         self.df = upload_fab_sites()
         self.api_by_cis = get_api_by_cis()
+        self.api_corresp_dict = get_api_correspondance()
 
 
-def get_api_correspondance():
+def get_api_correspondance() -> [DefaultDict, List]:
     """
     Excel database: CIS -> api
     BDPM: CIS -> [api1, api2, ...] (referential)
@@ -24,7 +27,7 @@ def get_api_correspondance():
     """
     cis_list = df.cis.unique()
     bad_cis = []
-    api_corresp_dict = defaultdict(list)  # {api_df: [api_bdpm]}
+    api_corresp_dict = defaultdict(list)  # {api_excel: [api_bdpm]}
     for cis in tqdm(cis_list):
         for api in df[df.cis == cis].substance_active:
             try:
@@ -33,30 +36,30 @@ def get_api_correspondance():
                 if cis not in bad_cis:
                     bad_cis.append(cis)
                 continue
-    return api_corresp_dict
+    return api_corresp_dict, bad_cis
 
 
-def clean_string(text):
+def clean_string(text: str) -> str:
     text = ''.join([word for word in text if word not in string.punctuation])
     text = text.lower()
     text = ' '.join([word for word in text.split() if word not in STOPWORDS])
+    text = unidecode.unidecode(text)
     return text
 
 
-def get_vectors(sentences):
+def get_vectors(sentences: Tuple[str]) -> np.ndarray:
     """
     Convert a collection of sentences to a matrix of token counts
     :param sentences: text
     :return:
     """
-    cleaned = list(map(clean_string, sentences))
     vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(cleaned)
+    X = vectorizer.fit_transform(sentences)
     vectors = X.toarray()
     return vectors
 
 
-def cosine_sim_vectors(vec1, vec2):
+def cosine_sim_vectors(vec1: np.ndarray, vec2: np.ndarray) -> np.float64:
     """
     Compute the similarity between two vectors
     :param vec1: api (Excel)
@@ -68,16 +71,32 @@ def cosine_sim_vectors(vec1, vec2):
     return cosine_similarity(vec1, vec2)[0][0]
 
 
-def get_api_similarities():
+def get_api_similarities() -> DefaultDict:
     """
-    Get for each api_excel, its similarity with the corresponding api_bdpm
+    Get for each api_excel, its similarity with the corresponding api
+    contained in the api_bdpm_list
     :return:  dict of dict
     """
-    api_corresp_dict = get_api_correspondance()
     api_sim_dict = defaultdict(dict)
-    for api_k, api_values in tqdm(api_corresp_dict.items()):
-        for api_v in api_values:
-            sentences = [api_k, api_v]
-            vectors = get_vectors(sentences)
-            api_sim_dict[api_k][api_v] = cosine_sim_vectors(vectors[0], vectors[1])
+    for api_excel, api_bdpm in tqdm(api_corresp_dict.items()):
+        for api in api_bdpm:
+            # Create sentences tuple
+            api_tuple = (api_excel, api)
+            # Clean the words in api_tuple
+            cleaned = tuple(map(clean_string, api_tuple))
+            # Vectorize
+            vectors = get_vectors(cleaned)
+            # Compute cosine similarity between the 2 words in tuple
+            api_sim_dict[api_excel][api] = cosine_sim_vectors(vectors[0], vectors[1])
     return api_sim_dict
+
+
+def get_most_sim_api(api_sim_dict: DefaultDict) -> Dict:
+    """
+    Pick the api_bdpm with the highest similarity score
+    :return: dict
+    """
+    return {
+        api_excel: max(api_sim_dict[api_excel], key=api_sim[api_excel].get)
+        for api_excel in api_corresp_dict.keys()
+    }
