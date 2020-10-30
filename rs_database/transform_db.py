5f36c36a848be768059054e2336068cfb86a4776
lrahal <line_r@hotmail.fr>
2020-10-30 12:00:24 +0100

    Add main function + manage typing

--- rs_database/transform_db.py
+++ rs_database/transform_db.py
@@ -1,7 +1,11 @@
+import csv
 import string
 import unidecode
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
 from nltk.corpus import stopwords
+from collections import defaultdict
 from typing import Tuple, List, Dict, DefaultDict
 from upload_db import upload_fab_sites, get_api_by_cis
 from sklearn.metrics.pairwise import cosine_similarity
@@ -10,18 +14,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 STOPWORDS = stopwords.words('french')
 
 
-class FabricationSites:
-    def __init__(self):
-        self.df = upload_fab_sites()
-        self.api_by_cis = get_api_by_cis()
-        self.api_corresp_dict = get_api_correspondance()
-
-
-def get_api_correspondance() -> [DefaultDict, List]:
+def get_api_correspondance(df: pd.DataFrame, api_by_cis: Dict) -> Tuple[DefaultDict, List]:
     """
     Excel database: CIS -> api
     BDPM: CIS -> [api1, api2, ...] (referential)
-    Make the correspondance api (Excel) -> api (BDPM) in order to put the good syntax
+    Make the correspondence api (Excel) -> api (BDPM) in order to put the good syntax
     in the MySQL database
     :return: dict of list
     """
@@ -71,7 +68,7 @@ def cosine_sim_vectors(vec1: np.ndarray, vec2: np.ndarray) -> np.float64:
     return cosine_similarity(vec1, vec2)[0][0]
 
 
-def get_api_similarities() -> DefaultDict:
+def get_api_similarities(api_corresp_dict: DefaultDict) -> DefaultDict:
     """
     Get for each api_excel, its similarity with the corresponding api
     contained in the api_bdpm_list
@@ -97,6 +94,40 @@ def get_most_sim_api(api_sim_dict: DefaultDict) -> Dict:
     :return: dict
     """
     return {
-        api_excel: max(api_sim_dict[api_excel], key=api_sim[api_excel].get)
+        api_excel: max(api_sim_dict[api_excel], key=api_sim_dict[api_excel].get)
         for api_excel in api_corresp_dict.keys()
     }
+
+
+def main():
+    # Upload dataframe from fabrication_sites table
+    df = upload_fab_sites()
+
+    # Get api by CIS from BDPM CIS_COMPO_bdpm.txt file
+    api_by_cis = get_api_by_cis()
+
+    # Find correspondence between Excel api and BDPM api
+    # 1 -> N
+    print('Finding correspondances between Excels api and BDPM api...')
+    api_corresp_dict, bad_cis = get_api_correspondance(df, api_by_cis)
+    print('{} CIS codes over {} are not referenced in the BDPM'.format(
+        len(bad_cis), len(df.cis.unique())), end='\n')
+
+    # Get the cosine similarity score for each couple (api_excel, api_bdpm)
+    print('Computing similarity scores...', end='\n')
+    api_sim_dict = get_api_similarities(api_corresp_dict)
+
+    # Save the one with the highest score
+    print('Storing best matches...', end='\n')
+    best_match_api = get_most_sim_api(api_sim_dict)
+
+    with open('../data/best_match_api.csv', 'w') as f:
+        w = csv.writer(f, delimiter=';')
+        w.writerow(['substance_active_excel', 'substance_active_bdpm'])
+        for key, value in best_match_api.items():
+            w.writerow([key, value])
+    print('best_match_api.csv printed!')
+
+
+if __name__ == '__main__':
+    main()
