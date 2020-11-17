3e5abf143d6ec9d6d8431a6041f62d5eb1c67859
lrahal <line_r@hotmail.fr>
2020-11-17 15:14:32 +0100

    Start Dash dashboard

---
+++ map_making/get_maps_data.py
@@ -0,0 +1,96 @@
+from typing import Tuple, List, Dict
+
+import pandas as pd
+
+from create_database.upload_db import upload_table_from_db
+
+
+def get_data(table_name: str, path: str, site_col: str) -> pd.DataFrame:
+    """
+    Get dataframe listing active substances, fabrication sites, countries, etc.
+    :param table_name: should be 'fabrication_sites'
+    :param path: 'map_making/data/sites_fabrication_substance_active.csv'
+    :param site_col: 'sites_fabrication_substance_active'
+    """
+    # Upload dataframe from fabrication_sites table
+    df_table = upload_table_from_db(table_name)
+    df_table.substance_active_match = df_table.apply(
+        lambda x: x.substance_active if pd.isnull(x.substance_active_match) else x.substance_active_match, axis=1)
+
+    # Get locations of each address in df_table (from csv)
+    df_locations = pd.read_csv(path)
+    df_locations = df_locations.drop(columns=['Unnamed: 0'])
+    df_locations = df_locations.rename(columns={'input_string': site_col})
+
+    # Merge both dataframes
+    df_final = pd.merge(
+        df_table[['denomination_specialite', 'cis', 'substance_active_match', site_col]],
+        df_locations, on=site_col, how='inner'
+    )
+
+    # Rename column substance_active_match
+    df_final = df_final.rename(columns={'substance_active_match': 'substance_active'})
+    return df_final
+
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove NaN countries
+    """
+    return df[~df['country'].isna()]
+
+
+def upload_countries_loc(path: str) -> pd.DataFrame:
+    """
+    Load countries locations csv and put it in dataframe
+    :param path: 'map_making/data/countries_locations.csv'
+    """
+    df_countries_loc = pd.read_csv(path)
+    df_countries_loc = df_countries_loc.drop(columns=['Unnamed: 0'])
+    return df_countries_loc
+
+
+def get_api_by_country(df: pd.DataFrame, path: str) -> pd.DataFrame:
+    """
+    Get number of active substances produced in each country
+    :param path: 'map_making/data/countries_locations.csv'
+    """
+    df_grouped_by_country = df.groupby('country')
+    df_grouped_by_country = df_grouped_by_country.agg({'substance_active': 'nunique'})
+    df_grouped_by_country = df_grouped_by_country.reset_index()
+
+    df_countries_loc = upload_countries_loc(path)
+
+    df_countries = pd.merge(
+        df_grouped_by_country[['substance_active', 'country']], df_countries_loc, on='country', how='inner'
+    )
+    df_countries = df_countries[~df_countries.country.isna()]
+    return df_countries
+
+
+def get_single_site_api(df: pd.DataFrame) -> List[Dict]:
+    """
+    Get list of dicts containing active substances
+    produced in only one place in the world
+    """
+    df_grouped_by_api = df.groupby('substance_active')
+    df_grouped_by_api = df_grouped_by_api.agg({'formatted_address': 'nunique'})
+    df_grouped_by_api = df_grouped_by_api.reset_index()
+
+    single_site_api = df_grouped_by_api[df_grouped_by_api.formatted_address == 1].substance_active.unique()
+
+    single_site_api_list = df[
+        df.substance_active.isin(single_site_api)][['substance_active', 'formatted_address', 'country']
+    ].to_dict(orient='records')
+    return list({v['substance_active']: v for v in single_site_api_list}.values())
+
+
+def get_final_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df = get_data('fabrication_sites',
+                  'map_making/data/sites_fabrication_substance_active.csv',
+                  'sites_fabrication_substance_active')
+    df = clean_data(df)
+
+    df_countries = get_api_by_country(df, 'map_making/data/countries_locations.csv')
+
+    return df, df_countries
---
+++ map_making/map_dashboard.py
@@ -0,0 +1,155 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import plotly.express as px
+from dash.dependencies import Input, Output
+
+from map_making.get_maps_data import get_final_dataframe, get_single_site_api
+
+external_scripts = [
+    {
+        'src': 'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+        'integrity': '',
+        'crossorigin': 'anonymous'
+    },
+    {
+        'src': 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js',
+        'integrity': '',
+        'crossorigin': 'anonymous'
+    }
+]
+
+# external CSS stylesheets
+external_stylesheets = [
+    {
+        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'type': 'text/css',
+        'integrity': '',
+        'crossorigin': 'anonymous'
+    }
+]
+
+
+app = dash.Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets)
+app.title = 'Active substances fabrication maps'
+
+df, df_countries = get_final_dataframe()
+df_countries['text'] = df_countries['country'] + ': ' + df_countries['substance_active'].astype(str)
+
+single_site_api_list = get_single_site_api(df)
+
+fig_world_map = px.scatter_geo(df_countries,
+                               # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
+                               lon='longitude',
+                               lat='latitude',
+                               # style
+                               color='country',
+                               size='substance_active',
+                               # choose the map chart's projection
+                               projection='natural earth',
+                               # columns which is in bold in the pop-up
+                               hover_name='text',
+                               # format of the pop-up not to display these columns' data
+                               hover_data={
+                                   'country': False, 'longitude': False, 'latitude': False, 'substance_active': False
+                               }
+                               )
+fig_world_map.update_geos(fitbounds='locations', showcountries=True)
+fig_world_map.update_layout(clickmode='event+select')
+
+# fig_world_map = go.Figure(data=go.Scattergeo(
+#     lon=df_countries['longitude'],
+#     lat=df_countries['latitude'],
+#     text=df_countries['text'],
+#     mode='markers',
+#     marker_color=df_countries['substance_active'],
+# ))
+#
+# fig_world_map.update_layout(
+#         title='Most trafficked US airports<br>(Hover for airport names)',
+#         geo_scope='world',
+#     )
+
+columns = ['denomination_specialite', 'cis', 'substance_active', 'country']
+
+app.layout = html.Div([
+    html.Div([
+        html.H1('Active substance fabrication sites worldwide')]),
+    dcc.Graph(id='world-graph', figure=fig_world_map),
+    dash_table.DataTable(
+        columns=[{'name': col, 'id': col} for col in columns],
+        data=[],
+        id='click-data',
+        page_size=10,
+        style_table={'height': '300px', 'overflowY': 'auto'},
+        style_as_list_view=True,
+        style_cell={'padding': '5px', 'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+    ),
+
+    html.Div([html.H1('Active substances having an only fabrication site')],
+             style={'textAlign': 'center', 'padding-bottom': '30'}),
+    dash_table.DataTable(
+        columns=[{'name': col, 'id': col} for col in ['substance_active', 'formatted_address', 'country']],
+        data=single_site_api_list,
+        filter_action='native',
+        sort_action='native',
+        page_size=10,
+        style_table={'height': '300px', 'overflowY': 'auto'},
+        style_as_list_view=True,
+        style_cell={'padding': '5px', 'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+    ),
+
+    html.Div([html.H1('Fabrication sites for a particular active substance')],
+             style={'textAlign': 'center', 'padding-bottom': '30'}),
+    dcc.Dropdown(
+        id='api-dropdown',
+        options=[{'label': api, 'value': api} for api in sorted(df.substance_active.unique())],
+        value='abacavir'
+    ),
+    dcc.Graph(id='api-graph')
+], className='container-sm')
+
+
+@app.callback(Output('click-data', 'data'), Input('world-graph', 'clickData'))
+def update_table(clickData):
+    if not clickData:
+        return []
+    country_name = clickData['points'][0]['customdata'][0]
+    print(country_name)
+    return df[df.country == country_name].sort_values(by=['substance_active']).to_dict('records')
+
+
+@app.callback(Output('api-graph', 'figure'), [Input('api-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    # set up the chart from the df dataframe
+    fig = px.scatter_geo(df[df.substance_active == selected_dropdown_value],
+                         # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
+                         lon='longitude',
+                         lat='latitude',
+                         # style
+                         color='country',
+                         # choose the map chart's projection
+                         projection='natural earth',
+                         # columns which is in bold in the pop-up
+                         hover_name='cis',
+                         # format of the pop-up not to display these columns' data
+                         hover_data={'cis': False, 'longitude': False, 'latitude': False}
+                         )
+    fig.update_traces(marker=dict(size=10))
+    fig.update_geos(fitbounds='locations', showcountries=True)
+    fig.update_layout(transition_duration=500)
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
