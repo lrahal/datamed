b4b8128249e3deecbb7a2ac05de2d060dd65f956
lrahal <line_r@hotmail.fr>
2020-11-20 17:57:30 +0100

    Refacto

--- map_making/notebooks/scripts/api_fab_sites_analysis.py
+++ map_making/notebooks/scripts/api_fab_sites_analysis.py
@@ -32,49 +32,32 @@ df = clean_data(df)
 # In[3]:
 
 
-len(df)
+len(df), len(df.cis.unique())
 
 
 # In[4]:
 
 
-len(df.cis.unique())
-
-
-# In[5]:
-
-
 df.head(2)
 
 
-# In[6]:
+# In[5]:
 
 
 # Keep rows having the right format (8 digits) (88% of all rows)
 df = df[~df.cis.isna()]
 df = df[df.cis.apply(lambda x: x.isdigit() and len(x) == 8)]
 
-
-# In[7]:
-
-
 len(df)
 
 
-# In[8]:
-
-
-19005 - 18000
-
-
-# In[9]:
+# In[6]:
 
 
 countries = sorted(df.country.unique())
-countries
 
 
-# In[10]:
+# In[7]:
 
 
 europe_countries = ['Germany', 'Belgium', 'Austria', 'Bulgaria', 'Croatia', 'Denmark', 'Spain', 'Estonia', 'Finland',
@@ -89,7 +72,7 @@ oecd_countries = ['Germany', 'Australia', 'Austria', 'Belgium', 'Canada', 'Chile
 third_countries = [c for c in countries if c not in europe_countries]
 
 
-# In[11]:
+# In[8]:
 
 
 df['europe'] = df.country.isin(europe_countries)
@@ -97,19 +80,15 @@ df['oecd'] = df.country.isin(oecd_countries)
 df['third'] = df.country.isin(third_countries)
 
 
-# In[ ]:
+# # Regroupement par code CIS
 
-
-
-
-
-# In[12]:
+# In[9]:
 
 
 df2 = pd.DataFrame(sorted(df.cis.unique()), columns=['cis'])
 
 
-# In[13]:
+# In[10]:
 
 
 df2['europe'] = df2.apply(lambda x: True in df[df.cis == x.cis].europe.to_list(), axis=1)
@@ -117,13 +96,13 @@ df2['oecd'] = df2.apply(lambda x: True in df[df.cis == x.cis].oecd.to_list(), ax
 df2['third'] = df2.apply(lambda x: True in df[df.cis == x.cis].third.to_list(), axis=1)
 
 
-# In[14]:
+# In[11]:
 
 
 df[df.cis == '69974276']
 
 
-# In[15]:
+# In[12]:
 
 
 df2['europe_only'] = df2.apply(lambda x: x.europe and not x.third, axis=1)
@@ -131,25 +110,25 @@ df2['third_only'] = df2.apply(lambda x: x.third and not x.europe, axis=1)
 df2['mix'] = df2.apply(lambda x: x.europe and x.third, axis=1)
 
 
-# In[16]:
+# In[13]:
 
 
 df2.head()
 
 
-# In[17]:
+# In[14]:
 
 
 len(df2[df2.europe_only]) + len(df2[df2.third_only]) + len(df2[df2.mix]), len(df.cis.unique())
 
 
-# In[18]:
+# In[15]:
 
 
 df2[(df2.europe == False) & (df2.third == False)]
 
 
-# In[19]:
+# In[16]:
 
 
 def get_category(x):
@@ -161,19 +140,21 @@ def get_category(x):
         return 'Europe & Third'
 
 
-# In[20]:
+# In[17]:
 
 
 df2['category'] = df2.apply(lambda x: get_category(x), axis=1)
 
 
-# In[21]:
+# In[18]:
 
 
 df2.head()
 
 
-# In[22]:
+# # Graphiques
+
+# In[19]:
 
 
 sns.set_style("white")
@@ -187,40 +168,38 @@ ax = sns.countplot(x='category',
                    edgecolor=sns.color_palette('dark', 3))
 
 
-# In[23]:
+# In[20]:
 
 
-corresp_dict = {'45': str(len(df2[df2.europe_only])) + ' (45.85%)',
-                '47': str(len(df2[df2.third_only])) + ' (47.58%)',
-                '6': str(len(df2[df2.mix])) + ' (6.57%)'}
+corresp_dict = {
+    '45': str(len(df2[df2.europe_only])) + ' (' + str(round(len(df2[df2.europe_only])/len(df2) * 100, 2)) +'%)',
+    '47': str(len(df2[df2.third_only])) + ' (' + str(round(len(df2[df2.third_only])/len(df2) * 100, 2)) +'%)',
+    '6': str(len(df2[df2.mix])) + ' (' + str(round(len(df2[df2.mix])/len(df2) * 100, 2)) +'%)'
+}
 
 
-# In[24]:
+# In[21]:
 
 
 plt.figure(figsize=(15, 10))
 df2['category'].value_counts(normalize=True).plot(kind='pie', autopct=lambda x: corresp_dict[str(math.floor(x))])
 
 
-# In[25]:
+# In[22]:
 
 
 df2
 
 
-# In[ ]:
+# # Export dans un csv
 
-
-
-
-
-# In[26]:
+# In[23]:
 
 
 df_spe = upload_table_from_db('cis_specialite')
 
 
-# In[27]:
+# In[24]:
 
 
 # Merge both dataframes
@@ -230,7 +209,7 @@ df_final = pd.merge(
 )
 
 
-# In[28]:
+# In[25]:
 
 
 df_final.to_csv('../data/api_fab_sites_repartition.csv', index=False, sep=';')
