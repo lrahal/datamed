4e5465651d9905ca52de6950bf6d2c60b7d74b0a
lrahal <line_r@hotmail.fr>
2020-11-30 18:34:06 +0100

    Work

--- map_making/get_fab_amount_map.py
+++ map_making/get_fab_amount_map.py
@@ -19,12 +19,47 @@ def create_fab_sites_api_amount_table():
                                  autocommit=True)
     cursor = connection.cursor()
 
-    sql = 'CREATE TABLE fab_sites_api_amount AS ' \
-          'SELECT t2.cis, t2.substance_active, t2.sites_fabrication_substance_active, t2.boites, ' \
-          'afsg.latitude, afsg.longitude, afsg.country ' \
-          'FROM api_fab_sites_geocode AS afsg ' \
+    sql = 'CREATE TABLE fabrication_sites_boites AS ' \
+          'SELECT t2.cis, t2.api, t2.sites_fabrication_substance_active, t2.boites, ' \
+          'asa.latitude, asa.longitude, asa.country ' \
+          'FROM ' \
+          '(SELECT fs.cis, ' \
+          'CASE WHEN substance_active_match IS NULL THEN fs.substance_active ELSE substance_active_match END ' \
+          'AS api, fs.sites_fabrication_substance_active, t1.boites ' \
+          'FROM fabrication_sites AS fs ' \
           'LEFT JOIN ' \
-          '(SELECT fs.cis, fs.substance_active, fs.sites_fabrication_substance_active, t1.boites ' \
+          '(SELECT cis_cip.cis as cis, SUM(open_medic.boites) AS boites ' \
+          'FROM open_medic ' \
+          'LEFT JOIN cis_cip ON open_medic.cip13 = cis_cip.cip13 ' \
+          'GROUP BY cis) as t1 ' \
+          'ON fs.cis = t1.cis) AS t2 ' \
+          'LEFT JOIN api_sites_addresses AS asa ' \
+          'ON t2.sites_fabrication_substance_active = asa.input_string'
+    cursor.execute(sql)
+
+
+    sql = 'CREATE TABLE prout AS ' \
+          'SELECT fs.cis, ' \
+          'CASE WHEN substance_active_match IS NULL THEN fs.substance_active ELSE substance_active_match END ' \
+          'AS api, fs.sites_fabrication_substance_active, t1.boites ' \
+          'FROM fabrication_sites AS fs ' \
+          'LEFT JOIN ' \
+          '(SELECT cis_cip.cis as cis, SUM(open_medic.boites) AS boites ' \
+          'FROM open_medic ' \
+          'LEFT JOIN cis_cip ON open_medic.cip13 = cis_cip.cip13 ' \
+          'GROUP BY cis) as t1 ' \
+          'ON fs.cis = t1.cis'
+    cursor.execute(sql)
+
+
+    sql = 'CREATE TABLE fab_sites_api_amount AS ' \
+          'SELECT t2.cis, t2.api, t2.sites_fabrication_substance_active, t2.boites, ' \
+          '    asa.latitude, asa.longitude, asa.country ' \
+          '    FROM api_sites_addresses AS asa ' \
+          'LEFT JOIN ' \
+          '(SELECT fs.cis, ' \
+          'CASE WHEN substance_active_match IS NULL THEN fs.substance_active ELSE substance_active_match END ' \
+          'AS api, fs.sites_fabrication_substance_active, t1.boites ' \
           'FROM fabrication_sites AS fs ' \
           'LEFT JOIN ' \
           '(SELECT cis_cip.cis as cis, SUM(open_medic.boites) AS boites ' \
@@ -32,9 +67,9 @@ def create_fab_sites_api_amount_table():
           'LEFT JOIN cis_cip ON open_medic.cip13 = cis_cip.cip13 ' \
           'GROUP BY cis) as t1 ' \
           'ON fs.cis = t1.cis ' \
-          'GROUP BY fs.cis, fs.substance_active, fs.sites_fabrication_substance_active, t1.boites) ' \
+          'GROUP BY fs.cis, api, fs.sites_fabrication_substance_active, t1.boites) ' \
           'AS t2 ' \
-          'ON t2.sites_fabrication_substance_active = afsg.input_string'
+          'ON t2.sites_fabrication_substance_active = asa.input_string'
     cursor.execute(sql)
 
 
