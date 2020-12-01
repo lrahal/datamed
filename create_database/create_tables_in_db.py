0c3377ff0297da3d55d6e7fa8dfbc7a3916c50f2
lrahal <line_r@hotmail.fr>
2020-12-01 17:12:19 +0100

    Create Excels dataframe from scratch without using db

--- create_database/create_tables_in_db.py
+++ create_database/create_tables_in_db.py
@@ -4,8 +4,10 @@ from typing import List, Dict
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 
+from .jade_analysis import build_api_fab_sites_dataframe
 from .models import connect_db, Specialite, SubstanceActive, Presentation, Consommation, Fabrication, Production
-from .upload_db import (upload_table_from_db, upload_cis_from_rsp, upload_compo_from_rsp, upload_cis_cip_from_bdpm)
+from .transform_db import compute_best_matches
+from .upload_db import upload_table_from_db, upload_cis_from_rsp, upload_compo_from_rsp, upload_cis_cip_from_bdpm
 
 # Credentials to database connection
 HOSTNAME = 'localhost'
@@ -14,11 +16,56 @@ UNAME = 'root'
 MYSQL_PWD = os.environ.get('MYSQL_PWD')
 
 
+def get_api_by_cis() -> Dict:
+    """
+    Get substance_active (API) list for each CIS
+    :return: dict of list
+    """
+    # Load dataframe
+    df = upload_compo_from_rsp('./create_database/data/RSP/COMPO_RSP.txt')
+    # List CIS codes
+    cis_list = df.cis.unique()
+    return {
+        str(cis): list(df[df.cis == cis].substance_active.unique())
+        for cis in cis_list
+    }
+
+
+def get_excels_df() -> pd.DataFrame:
+    """
+    Concatenate Excel files
+    Compute cosine similarity
+    :return:
+    """
+    path = './create_database/data/jade_final/'
+    # Load dataframe
+    print('Loading dataframe from concatenated Excel files...')
+    df = build_api_fab_sites_dataframe(path)
+
+    # Get api by CIS from RSP COMPO.txt file
+    api_by_cis = get_api_by_cis()
+
+    # Compute best match api using RSP
+    best_match_list = compute_best_matches(df, api_by_cis)
+    df_match = pd.DataFrame(best_match_list)
+    df_match.to_csv('./create_database/data/best_match_api.csv', index=False, sep=';')
+    print('best_match_api.csv printed!')
+
+    best_match_dict = {(m['cis'], m['excel']): {'api_rsp': m['rsp'], 'cos_sim': m['cos_sim']} for m in best_match_list}
+    df['substance_active_match'] = df.apply(
+        lambda x: best_match_dict[(x.cis, x.substance_active)]['api_rsp']
+        if (x.cis, x.substance_active) in best_match_dict else x.substance_active, axis=1)
+    df['cos_sim'] = df['substance_active_match'] = df.apply(
+        lambda x: best_match_dict[(x.cis, x.substance_active)]['cos_sim']
+        if (x.cis, x.substance_active) in best_match_dict else None, axis=1)
+    return df
+
+
 def get_api_list(df: pd.DataFrame) -> List[Dict]:
     api_list = df.substance_active_match.unique().tolist()
 
-    df_cis_compo = upload_compo_from_rsp('./create_database/data/COMPO_RSP.txt')
-    api_list.extend([api for api in df_cis_compo.substance_active.unique() if api not in api_list])
+    df_api = upload_compo_from_rsp('./create_database/data/RSP/COMPO_RSP.txt')
+    api_list.extend([api for api in df_api.substance_active.unique() if api not in api_list])
     api_list = list(filter(None, api_list))
     api_list = sorted(api_list)
     values_list = [{'name': api} for api in api_list]
@@ -28,8 +75,8 @@ def get_api_list(df: pd.DataFrame) -> List[Dict]:
 def get_cis_list(df: pd.DataFrame) -> List[Dict]:
     cis_list = df.cis.unique().tolist()
 
-    df_cis_compo = upload_cis_from_rsp('./create_database/data/CIS_RSP.txt')
-    cis_list.extend([str(cis) for cis in df_cis_compo.cis.unique() if str(cis) not in cis_list])
+    df_cis = upload_cis_from_rsp('./create_database/data/RSP/CIS_RSP.txt')
+    cis_list.extend([str(cis) for cis in df_cis.cis.unique() if str(cis) not in cis_list])
     cis_list = list(filter(None, cis_list))
     cis_list = sorted(cis_list)
     values_list = [{'cis': cis} for cis in cis_list]
@@ -37,12 +84,15 @@ def get_cis_list(df: pd.DataFrame) -> List[Dict]:
 
 
 def get_pres_list() -> List[Dict]:
+    """
+    CIS/CIP correspondence not found in RSP, but exists in BDPM
+    """
     df = upload_cis_cip_from_bdpm('./create_database/data/CIS_CIP_bdpm.txt')
     # df = df[~df.cip13.isna()]
     # df = df.astype({'cip13': int})
-    df_records = df.to_dict('records')
+    records = df.to_dict('records')
 
-    values_list = [{k: str(v) for k, v in zip(('cis', 'cip13'), (d['cis'], d['cip13']))} for d in df_records]
+    values_list = [{k: str(v) for k, v in zip(('cis', 'cip13'), (r['cis'], r['cip13']))} for r in records]
     return values_list
 
 
@@ -58,35 +108,59 @@ def get_conso_list() -> List[Dict]:
 def get_fabrication_list() -> List[Dict]:
     df = upload_table_from_db('api_sites_addresses')
     df = df.where(pd.notnull(df), None)
-    df_records = df.to_dict('records')
-    return [
-        {
-            k: v for k, v in zip(('address', 'latitude', 'longitude', 'country'),
-                                 (d['input_string'], d['latitude'], d['longitude'], d['country']))
-        }
-        for d in df_records
-    ]
-
-
-def get_prod_list(df: pd.DataFrame) -> List[Dict]:
-    df_records = df.to_dict('records')
     values_list = [
         {
-            k: v for k, v in zip(('cis', 'substance_active', 'address'),
-                                 (d['cis'], d['substance_active'], d['sites_fabrication_substance_active']))
+            k: v for k, v in zip(('address', 'latitude', 'longitude', 'country'),
+                                 (row['input_string'], row['latitude'], row['longitude'], row['country']))
         }
-        for d in df_records
-        if all((d['cis'], d['substance_active'], d['sites_fabrication_substance_active']))
+        for index, row in df.iterrows()
     ]
     return [dict(t) for t in {tuple(d.items()) for d in values_list}]
 
 
+def get_prod_list(df: pd.DataFrame) -> List[Dict]:
+    """
+    Create table listing all possible occurences of (cis, substance_active, address)
+    """
+    # Load substance_active table and join with dataframe
+    df_api = pd.read_sql_table('substance_active', connection)
+    df = df.merge(df_api, how='left', left_on='substance_active', right_on='name')
+    df = df.rename(columns={'id': 'substance_active_id'})
+
+    # Load fabrication table and join with dataframe
+    # df_fab = pd.read_sql_table('fabrication', connection)
+    df = df.merge(df_fab, how='left', left_on='sites_fabrication_substance_active', right_on='address')
+    df = df.rename(columns={'id': 'fabrication_id'})
+
+    # Replace NaN values with None
+    df = df.where(df.notnull(), None)
+
+    values_list = [
+        {
+            k: v for k, v in zip(('cis', 'substance_active_id', 'fabrication_id'),
+                                 (row['cis'], int(row['substance_active_id']), int(row['fabrication_id'])))
+        }
+        for index, row in df.iterrows()
+        if all((row['cis'], row['substance_active_id'], row['fabrication_id']))
+    ]
+    return [dict(t) for t in {tuple(d.items()) for d in values_list}]
+
+
+# def get_atc_list() -> List[Dict]:
+#     df = pd.read_csv('./create_database/data/ATC.csv', names=['cis', 'atc', 'v3'], delimiter=';', header=0)
+#     df = df.drop_duplicates()
+
+
+
 db = connect_db()  # establish connection
+connection = db.connect()
 Session = sessionmaker(bind=db)
 session = Session()
 
 
-def save_to_database_ORM(session):
+def save_to_database_orm(session):
+    # df = get_excels_df()
+
     df = upload_table_from_db('fabrication_sites')
     df['substance_active_match'] = df.apply(
         lambda x: x.substance_active if not x.substance_active_match else x.substance_active_match, axis=1)
