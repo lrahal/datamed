0adb52cfffbe42c5b8c2545f997306cb4ff53f1e
lrahal <line_r@hotmail.fr>
2021-01-04 17:48:04 +0100

    Refacto

--- map_making/notebooks/scripts/fabrication_sites_map.py
+++ map_making/notebooks/scripts/fabrication_sites_map.py
@@ -4,14 +4,16 @@
 # In[1]:
 
 
+import json
 import sys
 
+import folium
 import pandas as pd
 import plotly.express as px
+import requests
 
 sys.path.append('/Users/ansm/Documents/GitHub/datamed')
 
-from create_database.create_tables_in_db import get_excels_df
 from create_database.models import connect_db
 
 
@@ -20,21 +22,24 @@ from create_database.models import connect_db
 # In[2]:
 
 
-# Get dataframe from table
-df = get_excels_df()
-df = df.drop(['dci', 'type_amm', 'sites_production', 'sites_conditionnement_primaire',
-              'sites_conditionnement_secondaire', 'sites_importation', 'sites_controle',
-              'sites_echantillotheque', 'sites_certification', 'substance_active', 'pgp',
-              'filename', 'cos_sim'], axis=1)
-df = df.rename(columns={'substance_active_match': 'substance_active'})
-df = df.dropna(how='all')
-df = df.drop_duplicates()
+engine = connect_db()  # establish connection
+connection = engine.connect()
 
 
 # In[3]:
 
 
-len(df)
+df = pd.read_sql_table('production', connection)
+
+df = df.drop(['dci', 'type_amm', 'sites_production', 'sites_conditionnement_primaire',
+              'sites_conditionnement_secondaire', 'sites_importation', 'sites_controle',
+              'sites_echantillotheque', 'sites_certification', 'substance_active', 'pgp',
+              'filename'], axis=1)
+
+df = df.dropna(how='all')
+df = df.drop_duplicates()
+
+df.head(2)
 
 
 # In[4]:
@@ -65,48 +70,30 @@ addresses = df.sites_fabrication_substance_active.unique()
 # In[7]:
 
 
-engine = connect_db()  # establish connection
-connection = engine.connect()
+df_fab = pd.read_sql_table('fabrication', connection)
+
+df_fab.head(1)
 
 
 # In[8]:
 
 
-df_fab = pd.read_sql_table('fabrication', connection)
+df = df.merge(df_fab, left_on='fabrication_id', right_on='id', how='left')
+df = df.rename(columns={'id_x': 'id'})
+df = df.drop(['id_y', 'sites_fabrication_substance_active', 'fabrication_id'], axis=1)
+
+df.head(2)
 
 
 # In[9]:
 
 
-df_fab.head(1)
+# Retirer les geocodings qui n'ont pas fonctionné
+df = df[~df.country.isna()]
+len(df)
 
 
 # In[10]:
-
-
-df_fab_cis = pd.merge(
-    df[['cis', 'substance_active', 'sites_fabrication_substance_active', 'mitm']], df_fab,
-    left_on='sites_fabrication_substance_active', right_on='address', how='outer')
-
-df_fab_cis = df_fab_cis.drop(['id', 'sites_fabrication_substance_active'], axis=1)
-df_fab_cis.head(1)
-
-
-# In[11]:
-
-
-len(df), len(df_fab_cis)
-
-
-# In[12]:
-
-
-# Retirer les geocodings qui n'ont pas fonctionné
-df_fab_cis = df_fab_cis[~df_fab_cis.country.isna()]
-len(df_fab_cis)
-
-
-# In[13]:
 
 
 # list(df.substance_active)
@@ -115,11 +102,11 @@ len(df_fab_cis)
 
 # ## API fabrication sites by active substances (api)
 
-# In[14]:
+# In[11]:
 
 
 # set up the chart from the df dataFrame
-fig = px.scatter_geo(df_fab_cis, 
+fig = px.scatter_geo(df, 
                      # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
                      lon='longitude', 
                      lat='latitude',
@@ -128,9 +115,9 @@ fig = px.scatter_geo(df_fab_cis,
                      # choose the map chart's projection
                      projection='natural earth',
                      # columns which is in bold in the pop-up
-                     hover_name = 'substance_active',
+                     hover_name = 'substance_active_id',
                      # format of the pop-up not to display these columns' data
-                     hover_data = {'substance_active': False, 'longitude': False, 'latitude': False}
+                     hover_data = {'substance_active_id': False, 'longitude': False, 'latitude': False}
                     )
 #fig.update_traces(marker=dict(size=10, color='red'))
 fig.update_traces(marker=dict(size=10))
@@ -141,58 +128,55 @@ fig.show()
 
 # ## Number of api produced by country
 
+# In[12]:
+
+
+df_fab.head()
+
+
+# In[13]:
+
+
+df_api_by_country = df.groupby(['country']).agg({'substance_active_id': 'nunique'}).reset_index()
+
+
+# In[14]:
+
+
+countries = df_api_by_country.country.unique()
+
+
 # In[15]:
-
-
-df_grouped_by_country = df_fab_cis.groupby(['country']).agg({'substance_active': 'nunique'}).reset_index()
-
-
-# In[16]:
-
-
-countries = df_grouped_by_country.country.unique()
-
-
-# In[17]:
 
 
 # get_locations(countries, output_filename='countries_locations.csv')
 
 
-# In[18]:
+# In[16]:
 
 
 df_countries_loc = pd.read_csv('../data/countries_locations.csv')
-
-
-# In[19]:
-
-
 df_countries_loc.head()
 
 
-# In[20]:
+# In[17]:
 
 
 df_countries = pd.merge(
-    df_grouped_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
+    df_api_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
     on='country', how='left'
 )
 
-
-# In[21]:
-
-
-len(df_countries), len(df_grouped_by_country)
+len(df_countries), len(df_api_by_country)
 
 
-# In[22]:
+# In[18]:
 
 
 df_countries.head(1)
 
 
-# In[23]:
+# In[19]:
 
 
 # set up the chart from the df dataFrame
@@ -202,7 +186,7 @@ fig = px.scatter_geo(df_countries,
                      lat='latitude',
                      # style
                      color='country',
-                     size='substance_active',
+                     size='substance_active_id',
                      # choose the map chart's projection
                      projection='natural earth',
                      # columns which is in bold in the pop-up
@@ -215,98 +199,103 @@ fig.update_layout(title = 'Amount of active substances produced countries')
 fig.show()
 
 
+# In[56]:
+
+
+state_geo = 'world-countries.json'
+
+m = folium.Map(location=[48, 2], zoom_start=4)
+
+# Add the color for the chloropleth:
+folium.Choropleth(
+    geo_data=state_geo,
+    name='choropleth',
+    data=df_countries,
+    columns=['country', 'substance_active_id'],
+    key_on='feature.properties.name',
+    fill_color='BuPu',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Nb de substance actives fabriquees (spe commercialisees en France en 2019)'
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+
+m
+
+
 # ## Number of boxes sold in France which api have been produced in specific country
+
+# In[20]:
+
+
+df_ventes = pd.read_sql_table('ventes', connection)
+df_ventes['ventes_total'] = df_ventes.unites_officine + df_ventes.unites_hopital
+df_ventes.head(2)
+
+
+# In[21]:
+
+
+df.head(2)
+
+
+# In[22]:
+
+
+# Calculer les ventes par année et par CIS (somme sur les présentations)
+df_ventes_by_cis = df_ventes.groupby(['annee', 'cis']).agg({'ventes_total': 'sum'}).reset_index()
+
+
+# In[23]:
+
+
+# Calculer les ventes par CIS
+year = 2019
+
+df2 = df.merge(df_ventes_by_cis[df_ventes_by_cis.annee == year], on='cis', how='left')
+df2 = df2[df2.cis.notna()]
+df2 = df2.drop(columns=['titulaire_amm', 'denomination_specialite'])
+df2 = df2.drop_duplicates()
+
 
 # In[24]:
 
 
-db = connect_db()  # establish connection
-connection = db.connect()
+df2[df2.cis == '60349519']
 
 
 # In[25]:
 
 
-df_conso = pd.read_sql_table('consommation', connection)
-df_conso.head(2)
+# Calculer les ventes par pays
+df2_ventes_by_country = df2.groupby('country').agg({'ventes_total': 'sum'}).reset_index()
+
+df2_ventes_by_country.head(2)
 
 
 # In[26]:
 
 
-len(df_conso)
+countries = df2_ventes_by_country.country.dropna().unique()
 
 
 # In[27]:
 
 
-df.head(1)
-
-
-# In[28]:
-
-
-df_fab_cis.head(2)
-
-
-# In[29]:
-
-
-df2 = df_fab_cis.merge(df_conso, on='cis', how='left')
-df2 = df2[df2.cis.notna()]
-df2 = df2.drop_duplicates()
-
-
-# In[30]:
-
-
-len(df2), len(df_fab_cis)
-
-
-# In[31]:
-
-
-df2.head(2)
-
-
-# In[32]:
-
-
-df2_grouped_by_country = df2.groupby(['country']).agg(
-    {'ventes_officine': 'sum', 'ventes_hopital': 'sum', 'ventes_total': 'sum'}
-).reset_index() # 'substance_active': 'nunique'
-
-
-# In[33]:
-
-
-countries = df2_grouped_by_country.country.dropna().unique()
-
-
-# In[34]:
-
-
 df2_countries = pd.merge(
-    df2_grouped_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
+    df2_ventes_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
     on='country', how='left'
-)
+).sort_values(by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1)
 
+df2_countries.to_csv('../data/unités_vendues_par_pays_' + str(year) + '.csv', sep=';', index=False)
 
-# In[35]:
-
+df2_countries['ventes_total_millions'] = df2_countries.ventes_total.apply(lambda x: x/10**6)
 
 df2_countries.head(2)
 
 
-# In[36]:
-
-
-import folium
-import requests
-import json
-
-
-# In[37]:
+# In[28]:
 
 
 r = requests.get(
@@ -316,55 +305,30 @@ r = requests.get(
 a = json.loads(r.text)
 
 
-# In[38]:
+# In[29]:
 
 
-#with open('world-countries.json', 'w') as outfile:
+# with open('world-countries.json', 'w') as outfile:
 #    json.dump(a, outfile)
 
 
-# In[39]:
+# In[30]:
 
 
 c_list = [b['properties']['name'] for b in a['features']]
-
-
-# In[40]:
-
 
 for c in df2_countries.country.unique():
     if c not in c_list:
         print(c)
 
 
-# In[41]:
-
-
-df2_countries.sort_values(by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1).head()
-df2_countries.sort_values(
-    by=['ventes_total'], ascending=False).drop(
-    ['latitude', 'longitude'], axis=1).to_csv('../data/unités_vendues_par_pays_2018.csv', sep=';', index=False)
-
-
-# In[42]:
+# In[31]:
 
 
 # [b['properties']['name'] for b in a['features']]
 
 
-# In[43]:
-
-
-df2_countries['ventes_total_millions'] = df2_countries.ventes_total.apply(lambda x: x/1000000)
-
-
-# In[44]:
-
-
-df2_countries[df2_countries.country == 'United States']
-
-
-# In[45]:
+# In[32]:
 
 
 # https://github.com/python-visualization/folium/blob/master/examples/data/world-countries.json#
@@ -384,7 +348,7 @@ folium.Choropleth(
     fill_color='BuPu',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Unites (en millions) d API produites (spe vendues en France - 2018)'
+    legend_name='Unites (en millions) d API produites (spe vendues en France - ' + str(year) + ')'
 ).add_to(m)
 
 folium.LayerControl().add_to(m)
@@ -394,66 +358,56 @@ m
 
 # ## En pondérant par le nombre de substances actives contenues dans une spécialité
 
-# In[46]:
+# In[33]:
 
 
 df_pond = df2.copy()
 # df_pond = df_pond.drop_duplicates()
 
+df_pond.head()
 
-# In[47]:
+
+# In[34]:
 
 
-df_pond.ventes_officine = df_pond.apply(lambda x: x.ventes_officine/len(df_pond[df_pond.cis == x.cis]), axis=1)
-df_pond.ventes_hopital = df_pond.apply(lambda x: x.ventes_hopital/len(df_pond[df_pond.cis == x.cis]), axis=1)
 df_pond.ventes_total = df_pond.apply(lambda x: x.ventes_total/len(df_pond[df_pond.cis == x.cis]), axis=1)
 
 
-# In[48]:
+# In[35]:
 
 
-df_pond_grouped_by_country = df_pond.groupby(['country']).agg(
-    {'ventes_officine': 'sum', 'ventes_hopital': 'sum', 'ventes_total': 'sum'}
-).reset_index() # 'substance_active': 'nunique'
+df_ventes_pond_by_country = df_pond.groupby(['country']).agg({'ventes_total': 'sum'}).reset_index()
 
 
-# In[49]:
+# In[36]:
 
 
-countries = df_pond_grouped_by_country.country.unique()
+countries = df_ventes_pond_by_country.country.unique()
 
 
-# In[50]:
+# In[37]:
 
 
-df_pond_countries = pd.merge(
-    df_pond_grouped_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
+df_ventes_pond_countries = pd.merge(
+    df_ventes_pond_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
     on='country', how='left'
-)
+).sort_values(by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1)
+
+# Save into csv
+df_ventes_pond_countries.to_csv('../data/parts_vendues_par_pays_' + str(year) + '.csv', sep=';', index=False)
+
+df_ventes_pond_countries['ventes_total_millions'] = df_ventes_pond_countries.ventes_total.apply(lambda x: x/10**6)
+
+df_ventes_pond_countries.head(3)
 
 
-# In[51]:
+# In[38]:
 
 
-df_pond_countries.sort_values(by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1).head()
-df_pond_countries.sort_values(
-    by=['ventes_total'], ascending=False).drop(
-    ['latitude', 'longitude'], axis=1).to_csv('../data/parts_vendues_par_pays_2018.csv', sep=';', index=False)
+df_ventes_pond_countries[df_ventes_pond_countries.country == 'United States']
 
 
-# In[52]:
-
-
-df_pond_countries['ventes_total_millions'] = df_pond_countries.ventes_total.apply(lambda x: x/1000000)
-
-
-# In[53]:
-
-
-df_pond_countries[df_pond_countries.country == 'United States']
-
-
-# In[54]:
+# In[39]:
 
 
 state_geo = 'world-countries.json'
@@ -464,13 +418,13 @@ m = folium.Map(location=[48, 2], zoom_start=4)
 folium.Choropleth(
     geo_data=state_geo,
     name='choropleth',
-    data=df_pond_countries,
+    data=df_ventes_pond_countries,
     columns=['country', 'ventes_total_millions'],
     key_on='feature.properties.name',
     fill_color='BuPu',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Parts (millions) des API fabriquees / pays pour les spe vendues en France 2018'
+    legend_name='Parts (millions) des API fabriquees / pays pour les spe vendues en France ' + str(year)
 ).add_to(m)
 
 folium.LayerControl().add_to(m)
@@ -480,56 +434,40 @@ m
 
 # ## MITM only
 
-# In[55]:
+# In[40]:
 
 
 df_pond_mitm = df_pond[df_pond.mitm == 'oui']
 
 
-# In[56]:
+# In[41]:
 
 
-df_pond_mitm_grouped_by_country = df_pond_mitm.groupby(['country']).agg(
-    {'ventes_officine': 'sum', 'ventes_hopital': 'sum', 'ventes_total': 'sum'}
-).reset_index()
+df_ventes_pond_mitm_by_country = df_pond_mitm.groupby(['country']).agg({'ventes_total': 'sum'}).reset_index()
 
 
-# In[57]:
+# In[42]:
 
 
-countries_mitm = df_pond_mitm_grouped_by_country.country.dropna().unique()
+countries_mitm = df_ventes_pond_mitm_by_country.country.dropna().unique()
 
 
-# In[58]:
+# In[43]:
 
 
-df_pond_mitm_countries = pd.merge(
-    df_pond_mitm_grouped_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
+df_ventes_pond_mitm_countries = pd.merge(
+    df_ventes_pond_mitm_by_country, df_countries_loc[['latitude', 'longitude', 'country']],
     on='country', how='left'
-)
+).sort_values(by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1)
+
+df_ventes_pond_mitm_countries.to_csv('../data/parts_mitm_vendues_par_pays_' + str(year) + '.csv',
+                                     sep=';', index=False)
+
+df_ventes_pond_mitm_countries['ventes_total_millions'] = df_ventes_pond_mitm_countries.ventes_total.apply(
+    lambda x: x/10**6)
 
 
-# In[59]:
-
-
-df_pond_mitm_countries.sort_values(
-    by=['ventes_total'], ascending=False).drop(['latitude', 'longitude'], axis=1).head(7)
-
-
-# In[60]:
-
-
-df_pond_mitm_countries.sort_values(by=['ventes_total'], ascending=False).drop(
-    ['latitude', 'longitude'], axis=1).to_csv('../data/parts_mitm_vendues_par_pays_2018.csv', sep=';', index=False)
-
-
-# In[61]:
-
-
-df_pond_mitm_countries['ventes_total_millions'] = df_pond_mitm_countries.ventes_total.apply(lambda x: x/1000000)
-
-
-# In[62]:
+# In[44]:
 
 
 state_geo = 'world-countries.json'
@@ -540,13 +478,13 @@ m = folium.Map(location=[48, 2], zoom_start=4)
 folium.Choropleth(
     geo_data=state_geo,
     name='choropleth',
-    data=df_pond_mitm_countries,
+    data=df_ventes_pond_mitm_countries,
     columns=['country', 'ventes_total_millions'],
     key_on='feature.properties.name',
     fill_color='BuPu',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Parts (millions) des API fabriquees / pays pour les spe MITM vendues en France 2018'
+    legend_name='Parts (millions) des API fabriquees / pays pour les spe MITM vendues en France ' + str(year)
 ).add_to(m)
 
 folium.LayerControl().add_to(m)
@@ -554,19 +492,19 @@ folium.LayerControl().add_to(m)
 m
 
 
-# In[63]:
+# In[45]:
 
 
 # df2[df2.cis == '67705462']
 
 
-# In[64]:
+# In[46]:
 
 
 # df3[df3.cis == '67705462']
 
 
-# In[65]:
+# In[47]:
 
 
 # v = df3.cis.value_counts()
@@ -575,11 +513,11 @@ m
 
 # ## Repartition of API fabrication sites by particular API
 
-# In[66]:
+# In[48]:
 
 
 # set up the chart from the df dataFrame
-fig = px.scatter_geo(df_fab_cis[df_fab_cis.substance_active == 'hydrochlorothiazide'], 
+fig = px.scatter_geo(df[df.substance_active_id == 3297], 
                      # longitude is taken from the df['longitude'] columns and latitude from df['latitude']
                      lon='longitude', 
                      lat='latitude',
@@ -597,6 +535,30 @@ fig.update_traces(marker=dict(size=10))
 fig.update_geos(fitbounds='locations', showcountries=True)
 fig.update_layout(title = 'Fabrication sites of hydrochlorothiazide')
 fig.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
