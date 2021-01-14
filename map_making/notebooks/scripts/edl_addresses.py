8db8d85fd6515ba35c093cdcd091f04a6606cf4d
lrahal <line_r@hotmail.fr>
2021-01-14 10:09:53 +0100

    Use get_geoloc function

--- map_making/notebooks/scripts/edl_addresses.py
+++ map_making/notebooks/scripts/edl_addresses.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[8]:
+
 
 import re
+import sys
 import urllib
 
 import pandas as pd
 import requests
 import seaborn as sns
-from google.colab import drive
 
-drive.mount('/content/drive')
-
-
-# In[5]:
+sys.path.append('/Users/ansm/Documents/GitHub/datamed')
+from create_database.models import connect_db
+from map_making.get_geoloc import get_google_results
 
 
-df = pd.read_csv('drive/MyDrive/datamed/production.csv', delimiter=';')
+# In[9]:
+
+
+engine = connect_db()  # establish connection
+connection = engine.connect()
+
+
+# In[10]:
+
+
+df = pd.read_sql_table('production', connection)
 df = df[['id', 'substance_active_id', 'sites_fabrication_substance_active']]
 
 df['nb_semicolon'] = df.sites_fabrication_substance_active.str.count(';')
@@ -26,23 +36,29 @@ df['address_length'] = df.sites_fabrication_substance_active.apply(lambda x: len
 df.head(3)
 
 
-# In[4]:
+# In[12]:
 
 
-sns.scatterplot(data=df, x='address_length', y='nb_comma')
+sns.scatterplot(data=df, x='address_length', y='nb_semicolon')
 
 
-# In[60]:
+# In[18]:
+
+
+# address = df[df.address_length >= 500].iloc[0].sites_fabrication_substance_active
+address = "r0-cep 2016-290-rev 00; divi's laboratories ltd- unit ii annavaram post- chippada village, 531162 bheemunipatnam mandal, andhra pradesh, india r0-cep-2012-338 rev 02; novartis pharma stein ag schaffhauserstrasse 4332 stein switzerland ; divi’s laboratories limited unit-2, chippada (v), annavaram (post) bheemunipatnam (m) visakhapatnam district 531162 andhra pradesh, india ;"
+
+
+# In[19]:
+
+
+get_google_results(address)
+
+
+# In[ ]:
 
 
 BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
-
-
-# In[61]:
-
-
-#address = df[df.address_length >= 500].iloc[0].sites_fabrication_substance_active
-address = "r0-cep 2016-290-rev 00; divi's laboratories ltd- unit ii annavaram post- chippada village, 531162 bheemunipatnam mandal, andhra pradesh, india r0-cep-2012-338 rev 02; novartis pharma stein ag schaffhauserstrasse 4332 stein switzerland ; divi’s laboratories limited unit-2, chippada (v), annavaram (post) bheemunipatnam (m) visakhapatnam district 531162 andhra pradesh, india ;"
 
 
 # In[62]:
