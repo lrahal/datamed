df527cbeb2f75ca8dcf6b718074bbd8e5fe7af9f
lrahal <line_r@hotmail.fr>
2021-02-23 15:27:27 +0100

    Format + refacto

--- datamed_dash/apps/components/donnees_une.py
+++ datamed_dash/apps/components/donnees_une.py
@@ -1,10 +1,10 @@
 from dash.development.base_component import Component
-from dash_html_components import Div, A, H3, Span, Iframe, Img
+from dash_html_components import Div, A, H2, H3, Span, Iframe, Img
 
 
 def Indicateur(value, text, color, class_name):
     return Span(
-        [Span(str(value), style={'color': color, 'font-size': '20px'}), text],
+        [Span(str(value), style={'color': color}), text],
         className=class_name,
     )
 
@@ -12,13 +12,12 @@ def Indicateur(value, text, color, class_name):
 def DonneesUne() -> Component:
     src = '/assets/pills.jpg'
     return Div([
-        Span(
+        H2(
             'Les données à la une',
-            style={"font-size": "30px"},
             className='one'
         ),
         Indicateur(265, ' ruptures de médicaments', '#ff8c00', 'two grid-content'),
-        Indicateur(10, ' pays fabriquant du paracétamol', '#ff8c00', 'three grid-content'),
+        Indicateur(10, ' pays fabriquent du paracétamol', '#ff8c00', 'three grid-content'),
         Indicateur(1921, ' effets indésirables vaccin COVID 19', '#ff8c00', 'four grid-content'),
         Div([
             Img(
@@ -37,9 +36,9 @@ def DonneesUne() -> Component:
                 A(
                     id='link',
                     href='analyse_thematique_ruptures',
-                    children="VOIR L'ANALYSE THEMATIQUE",
+                    children="VOIR L'ANALYSE THÉMATIQUE",
                     target="_blank",
-                    className='d-block text-left'
+                    className='analyse-thematique d-block text-left'
                 ),
             ],
                 className='px-4 py-4 d-block'
@@ -60,7 +59,17 @@ def DonneesUne() -> Component:
             Indicateur(35, ' importations ce mois-ci', '#00bfff', 'd-block'),
             Indicateur(65, ' réapprovisionnements', '#00bfff', 'd-block'),
         ],
-            className='seven grid-content'
+            className='seven grid-content d-flex flex-column justify-content-between py-5'
+        ),
+        Div([
+            A('Répartition par sexe des patients traités au Doliprane')
+        ],
+            className='eight grid-content px-3 py-4',
+        ),
+        Div([
+            A('Répartition par âge des patients traités au Doliprane')
+        ],
+            className='nine grid-content px-3 py-4',
         ),
     ],
         className='donnees-une'
