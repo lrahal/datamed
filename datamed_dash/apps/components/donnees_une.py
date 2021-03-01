00286edd582781d742fac44231381a8e4ee6e2fc
lrahal <line_r@hotmail.fr>
2021-03-01 11:29:46 +0100

    Integrate green SVG background

--- datamed_dash/apps/components/donnees_une.py
+++ datamed_dash/apps/components/donnees_une.py
@@ -12,65 +12,78 @@ def Indicateur(value: int, text: str, color: str, class_name: str) -> Component:
 def DonneesUne() -> Component:
     src = '/assets/pills.jpg'
     return Div([
-        H2(
-            'Les données à la une',
-            className='one'
+        Div(
+            [],
+            className='top'
         ),
-        Indicateur(265, ' ruptures de médicaments', '#ff8c00', 'two grid-content'),
-        Indicateur(10, ' pays fabriquent du paracétamol', '#ff8c00', 'three grid-content'),
-        Indicateur(1921, ' effets indésirables vaccin COVID 19', '#ff8c00', 'four grid-content'),
         Div([
-            Img(
-                src=src,
-                style={'width': '100%'}
+            H2(
+                'Les données à la une',
             ),
             Div([
-                H3(
-                    "Qu'est-ce qu'une rupture ?",
-                    className='d-block text-left',
+                Indicateur(265, ' ruptures de médicaments', '#ff8c00', 'two grid-content'),
+                Indicateur(10, ' pays fabriquent du paracétamol', '#ff8c00', 'three grid-content'),
+                Indicateur(1921, ' effets indésirables vaccin COVID 19', '#ff8c00', 'four grid-content'),
+                Div([
+                    Img(
+                        src=src,
+                        style={'width': '100%'}
+                    ),
+                    Div([
+                        H3(
+                            "Qu'est-ce qu'une rupture ?",
+                            className='d-block text-left',
+                        ),
+                        P(
+                            "Déclaration ou réelle rupture ? Nos experts vous expliquent la différence avec des chiffres analysés",
+                            className='py-5 d-block text-left',
+                        ),
+                        A(
+                            id='link',
+                            href='analyse_thematique_ruptures',
+                            children="VOIR L'ANALYSE THÉMATIQUE",
+                            target="_blank",
+                            className='analyse-thematique d-block text-left'
+                        ),
+                    ],
+                        className='px-4 py-4 d-block'
+                    ),
+                ],
+                    className='five grid-content'
                 ),
-                P(
-                    "Déclaration ou réelle rupture ? Nos experts vous expliquent la différence avec des chiffres analysés",
-                    className='py-5 d-block text-left',
+                Div([
+                    Iframe(
+                        id='map',
+                        srcDoc=open('./assets/map.html', 'r').read()
+                    )
+                ],
+                    className='six grid-content'
                 ),
-                A(
-                    id='link',
-                    href='analyse_thematique_ruptures',
-                    children="VOIR L'ANALYSE THÉMATIQUE",
-                    target="_blank",
-                    className='analyse-thematique d-block text-left'
+                Div([
+                    Indicateur(265, ' ruptures de médicaments', '#00bfff', 'd-block'),
+                    Indicateur(35, ' importations ce mois-ci', '#00bfff', 'd-block'),
+                    Indicateur(65, ' réapprovisionnements', '#00bfff', 'd-block'),
+                ],
+                    className='seven grid-content d-flex flex-column justify-content-between py-5'
+                ),
+                Div([
+                    A('Répartition par sexe des patients traités au Doliprane')
+                ],
+                    className='eight grid-content px-3 py-4',
+                ),
+                Div([
+                    A('Répartition par âge des patients traités au Doliprane')
+                ],
+                    className='nine grid-content px-3 py-4',
                 ),
             ],
-                className='px-4 py-4 d-block'
+                className='donnees-une',
             ),
-        ],
-            className='five grid-content'
-        ),
-        Div([
-            Iframe(
-                id='map',
-                srcDoc=open('./assets/map.html', 'r').read()
-            )
-        ],
-            className='six grid-content'
-        ),
-        Div([
-            Indicateur(265, ' ruptures de médicaments', '#00bfff', 'd-block'),
-            Indicateur(35, ' importations ce mois-ci', '#00bfff', 'd-block'),
-            Indicateur(65, ' réapprovisionnements', '#00bfff', 'd-block'),
-        ],
-            className='seven grid-content d-flex flex-column justify-content-between py-5'
-        ),
-        Div([
-            A('Répartition par sexe des patients traités au Doliprane')
-        ],
-            className='eight grid-content px-3 py-4',
-        ),
-        Div([
-            A('Répartition par âge des patients traités au Doliprane')
-        ],
-            className='nine grid-content px-3 py-4',
+        ], className='donnees-une-content'),
+        Div(
+            [],
+            className='bottom'
         ),
     ],
-        className='donnees-une'
+        className='donnees-une-container'
     )
