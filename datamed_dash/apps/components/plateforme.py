f92c8a898a5eb41c5ab4a480c56bcbb9de254e1c
lrahal <line_r@hotmail.fr>
2021-03-23 17:51:44 +0100

    Generate Plateforme component using bootstrap element Card

--- datamed_dash/apps/components/plateforme.py
+++ datamed_dash/apps/components/plateforme.py
@@ -1,36 +1,39 @@
 from dash.development.base_component import Component
-from dash_bootstrap_components import Button
-from dash_html_components import Div, Span, Img, P
+from dash_bootstrap_components import Button, Card, CardImg, CardBody
+from dash_html_components import Div
 
 
 def Plateforme() -> Component:
-    src = "/assets/screen.jpg"
-    return Div(
+    return Card(
         [
-            Img(src=src, style={"width": "50%"}, className="img-fluid", alt="Responsive image"),
             Div(
                 [
-                    Span(
-                        "Une plateforme unique pour réunir les données essentielles de l'ANSM",
-                        className="heading-4",
-                    ),
-                    P(
-                        "Soucieuse de rendre ses données accessibles et transparentes, l'Agence Nationale de Sécurité "
-                        "du Médicament et des Produits de Santé a lancé data.ansm.fr, un lieu numérique pour retrouver "
-                        "les données autour de l'agence.",
-                        className="mt-3 color-black",
-                        style={"font-size": "20px", "font-weight": "400"},
-                    ),
-                    Button(
-                        "À PROPOS",
-                        outline=True,
-                        className="button-text-bold  mr-1",
-                        color="secondary",
-                        style={"margin-top": "1rem"},
+                    CardImg(src="/assets/screen.jpg", className="col-7 h-100"),
+                    CardBody(
+                        [
+                            Div(
+                                "Une plateforme unique pour réunir les données essentielles de l'ANSM",
+                                className="heading-4",
+                            ),
+                            Div(
+                                "Soucieuse de rendre ses données accessibles et transparentes, l'Agence Nationale de "
+                                "Sécurité du Médicament et des Produits de Santé a lancé data.ansm.fr, un lieu "
+                                "numérique pour retrouver les données de l'agence.",
+                                className="medium-text mt-3",
+                            ),
+                            Button(
+                                "À PROPOS",
+                                outline=True,
+                                className="button-text-bold",
+                                type="submit",
+                                color="secondary",
+                            ),
+                        ],
+                        className="col-5 plateforme-card-body",
                     ),
                 ],
-                className="px-3 py-3 mt-2",
+                className="row",
             ),
         ],
-        className="plateforme px-3 py-3 d-flex",
+        className="plateforme-card w-75 mx-auto",
     )
