00853c910e1d4d75fbb4c0648226c31c8730f527
lrahal <line_r@hotmail.fr>
2021-03-24 15:09:08 +0100

    Card for each exploration subject

--- datamed_dash/apps/components/explorer.py
+++ datamed_dash/apps/components/explorer.py
@@ -1,5 +1,6 @@
 from dash.development.base_component import Component
-from dash_html_components import Div
+from dash_html_components import Div, B
+from dash_bootstrap_components import Button, Card, CardImg, CardBody, CardLink
 
 
 def ExplorerHeader() -> Component:
@@ -20,5 +21,56 @@ def ExplorerHeader() -> Component:
     )
 
 
+def BddCard(src_img, title, open_data, body, source_bdd) -> Card:
+    return Card(
+        [
+            CardImg(src=src_img, style={"width": "270px", "height": "240px"}),
+            CardBody(
+                [
+                    Div(title, className="heading-6"),
+                    Div(["Open data : ", open_data], className="small-text"),
+                    Div(
+                        [
+                            Div(
+                                body,
+                                className="button-text d-inline-block col-6",
+                            ),
+                            Div(
+                                [
+                                    B("Source de données :"),
+                                    source_bdd,
+                                ],
+                                className="button-text d-inline-block col-6",
+                            ),
+                        ],
+                        className="d-flex row mt-4 mb-5",
+                    ),
+                    CardLink(
+                        "DÉCOUVRIR LE JEU DE DONNÉES",
+                        href="/apps/ruptures",
+                        className="normal-text link",
+                        id="link-donnees-ruptures",
+                    ),
+                ],
+                className="px-4",
+            ),
+        ],
+        className="explorer-card",
+        style={"width": "956px"},
+    )
+
+
 def Explorer() -> Component:
-    return Div([ExplorerHeader()])
+    return Div(
+        [
+            ExplorerHeader(),
+            BddCard(
+                "/assets/screen.jpg",
+                "Cartographie des sites de fabrication",
+                "Non",
+                "Surveillez les ruptures de stock et trouvez des alternatives thérapeutiques en fonction du profil du "
+                "patient concerné.",
+                "ANSM, Base de données publique des médicaments",
+            ),
+        ]
+    )
