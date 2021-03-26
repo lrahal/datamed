3d2e03282a54be0a1e628ceb6b7771fa9217ba2d
lrahal <line_r@hotmail.fr>
2021-03-26 11:43:25 +0100

    Change background color of boxes + add Pie and Curve layouts to layouts.py file

--- datamed_dash/apps/constants/layouts.py
+++ datamed_dash/apps/constants/layouts.py
@@ -14,10 +14,31 @@ BAR_LAYOUT = {
         tickcolor="white",
         ticklen=1,
     ),
-    "plot_bgcolor": "rgba(0,0,0,0)",
+    "plot_bgcolor": "#FAFAFA",
+    "paper_bgcolor": "#FAFAFA",
     "margin": dict(l=0, r=0, t=0, b=0),
     "barmode": "group",
     "bargap": 0.10,
     "bargroupgap": 0.0,
     "font": {"size": 12, "color": "black"},
+}
+
+CURVE_LAYOUT = {
+    "xaxis_showgrid": False,
+    "yaxis_showgrid": False,
+    "yaxis2_showgrid": False,
+    "hovermode": "x unified",
+    "plot_bgcolor": "#FAFAFA",
+    "paper_bgcolor": "#FAFAFA",
+    "margin": dict(t=0, b=0, l=0, r=0),
+    "font": {"size": 12, "color": "black"},
+    "legend": dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+}
+
+PIE_LAYOUT = {
+    "plot_bgcolor": "#FAFAFA",
+    "paper_bgcolor": "#FAFAFA",
+    "hovermode": False,
+    "margin": dict(t=0, b=0, l=0, r=0),
+    "legend": dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
 }
